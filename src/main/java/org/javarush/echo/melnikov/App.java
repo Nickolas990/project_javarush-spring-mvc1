15cc8e380680f633ca2821c48906be01440387ec
Nickolas990 <hrom_90@hotmail.com>
2023-03-10 10:30:19 +0300

    Initial commit

---
+++ src/main/java/org/javarush/echo/melnikov/App.java
@@ -0,0 +1,13 @@
+package org.javarush.echo.melnikov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
